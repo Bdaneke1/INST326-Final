04717e1df9e1f1a2e2d9cfa9c84f975002d652a6
jakefine3 <159298183+jakefine3@users.noreply.github.com>
2024-05-09 19:14:04 -0400

    Update Finance_Tracker.py

--- Finance_Tracker.py
+++ Finance_Tracker.py
@@ -23,61 +23,51 @@ class Finances:
         self.income = []
         self.expenses = []
         self.transactions = []
+        self.investments = {}
         
-    def add_income_source(self, source):
+    def add_income(self, source, amount, date):
         """
         Adds a new income source to the system
         
         Args:
             source (str): represents the name of the income source(job, etc.)
+            amount (float): $ amount of the income source
+            date (str): date associated with the income source
         """
-        self.income.append(source)
-        
-    def update_income_source(self, old_source, new_source):
-        """
-        Allows user to update existing income source
-        
-        Args:
-            old_source (str): existing income source
-            new_source (str): replacement income source
-        """
-        try:
-            index = self.income.index(old_source)
-            self.income[index] = new_source
-        except ValueError:
-            print("Income source not found.")
+        self.income.append({'source': source, 'amount': amount, 'date': date})
             
-    def add_expense_category(self, category):
+    def add_expense(self, category, amount, date):
         """
         Adds a new expense category to the system
         
         Args:
             category (str): represents the name of the expense category
+            amount (float): monetary amount of the expense category
+            date (str): date associated with the expense category
         """
-        self.expenses.append(category)
-
-    def update_expense_category(self, old_category, new_category):
-        """
-        Allows user to update the expense category
-        
-        Args:
-            old_category (str): existing expense category
-            new_category (str): replacement expense category
-        """
-        try:
-            index = self.expenses.index(old_category)
-            self.expenses[index] = new_category
-        except ValueError:
-            print("Expense category not found.")
+        self.expenses.append({'category': category, 'amount': amount, 'date': date})
             
-    def add_transaction(self, transaction):
+    def add_transaction(self, date, category, amount, description):
         """
-        Adds a new transaction to list
+        Adds a new transaction to the system
         
         Args:
-            transaction: the transaction to add
+            date (str): date of the transaction
+            category (str): category of the transaction (income, expense, savings)
+            amount (float): monetary amount of the transaction
+            description (str): description of the transaction
         """
-        self.transactions.append(transaction)
+        self.transactions.append({'date': date, 'category': category, 'amount': amount, 'description': description})
+        
+    def add_investment(self, asset, amount):
+        """
+        Adds a new asset/investment to the portfolio
+        
+        Args:
+            asset (str): name of the asset
+            amount (float): monetary cost of the asset
+        """
+        self.investments[asset] = amount
         
     def build_report(self, start_date, end_date):
         """
@@ -88,51 +78,46 @@ class Finances:
             end_date (str): end date of reporting period
         """
         report = {
-            'Income': [inc for inc in self.income if start_date <= inc.date <= end_date],
-            'Expenses': [exp for exp in self.expenses if start_date <= exp.date <= end_date],
-            'Transactions': [trans for trans in self.transactions if start_date <= trans.date <= end_date]
+            'Income': [inc for inc in self.income if start_date <= inc['date'] <= end_date],
+            'Expenses': [exp for exp in self.expenses if start_date <= exp['date'] <= end_date],
+            'Transactions': [tra for tra in self.transactions if start_date <= tra['date'] <= end_date],
+            'Investments': self.investments
         }
         
         return report
 
     #If we use a JSON for the data, this is how we would save the data to a JSON file
     def save_to_file(self, file_name):
-        with open(file_name, 'w') as file:
-            json.dump({'income': self.income, 'expenses': self.expenses}, file)
+        try:
+            with open(file_name, 'w') as file:
+                json.dump({'income': self.income, 'expenses': self.expenses}, file)
+            print("Data saved.")
+        except Exception as e:
+            print(f"An error occurred while saving: {e}")
 
-    #If we use a JSON for the finanical data, this will read it and update the clases 
+    #If we use a JSON for the financial data, this will read it and update the classes 
     def load_from_file(self, file_name):
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-            self.income = data['income']
-            self.expenses = data['expenses']
+        try:
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+                if 'income' in data:
+                    self.income = data['income']
+                if 'expenses' in data:
+                    self.expenses = data['expenses']
+                if 'transactions' in data:
+                    self.transactions = data['transactions']
+                if 'investments' in data:
+                    for inv in data['investments']:
+                        self.investments[inv['asset']] = inv['amount']
+            print("Data loaded.")
+        except FileNotFoundError:
+            print("File not found.")
+        except Exception as e:
+            print(f"An error occurred while loading data: {e}")
 
-class Transaction:
-    """
-    Represents each financial transaction the user makes.
-    """
-    
-    def __init__(self, date, category, amount, description):
-        """
-        Initializes each transaction with descriptors
-        
-        Args:
-            date (str): date of the transaction
-            category (str): income or expense or savings
-            amount (int): monetary amount of the transaction
-            description (str): description of the transaction
-        """
-        
-        self.date = date
-        self.category = category
-        self.amount = amount
-        self.description = description
-        
-    #add formatting function and potentially more to organize transactions
-    
 class Investments:
     """
-    Represents the user's financial portfolio
+    Represents the user's investments
     """
     
     def __init__(self, name):
@@ -166,14 +151,22 @@ class Investments:
             del self.investments[asset]
 
     def calculate_investment_growth(self, asset, growth_rate):
+        """
+        Calculates the growth of an investment
+        
+        Args:
+            asset (str): name of the asset
+            growth_rate (float): rate of growth (e.g., 0.05 for 5% growth)
+            
+        Returns:
+            float: updated investment amount after growth
+        """
         if asset in self.investments:
-            return self.investments[asset] * (1 + growth_rate)
+            self.investments[asset] *= (1 + growth_rate)
+            return self.investments[asset]
         else:
             print("Investment not found.")
             return None
-
-class CurrencyNotFoundError(Exception):
-    """Custom exception for indicating that the currency code was not found."""
 
 def convert_currency(amount, from_currency, to_currency):
     try:
@@ -183,58 +176,63 @@ def convert_currency(amount, from_currency, to_currency):
 
         #checks if from_currency and to_currency exists in the exchange rates data
         if from_currency not in data['rates']:
-            raise CurrencyNotFoundError(f"Currency code '{from_currency}' not found. Please use a valid currency code.")
+            raise Exception(f"Currency code '{from_currency}' not found. Please use a valid currency code.")
 
         rates = data['rates']
 
         if to_currency not in rates:
-            raise CurrencyNotFoundError(f"Currency code '{to_currency}' not found. Please use a valid currency code.")
+            raise Exception(f"Currency code '{to_currency}' not found. Please use a valid currency code.")
 
         return amount * rates[to_currency]
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
-    except CurrencyNotFoundError as e:
+    except Exception as e:
         print(f"Error: {e}")
         return None
 
 def main():
-    print("Welcome to your Financial Management System!")
+    print("Welcome to the Financial Management System!")
     
     finances = Finances()
     investments = Investments("My Portfolio")
     
     while True:
         print("\nOptions:")
-        print("1. Add Income Source")
-        print("2. Add Expense Category")
+        print("1. Add Income")
+        print("2. Add Expense")
         print("3. Add Transaction")
         print("4. Add Investment")
         print("5. Generate Report")
         print("6. Save Data to JSON")
         print("7. Load Data from JSON")
         print("8. Convert Currency")
-        print("9. View Income Sources")
-        print("10. View Expense Categories")
-        print("11. Quit")
+        print("9. View Income")
+        print("10. View Expenses")
+        print("11. View Transactions")
+        print("12. View Investments")
+        print("13. Quit")
 
         choice = input("Enter your choice: ")
 
         if choice == '1':
             source = input("Enter the name of the income source: ")
-            finances.add_income_source(source)
-            print("Income source added successfully.")
+            amount = float(input("Enter the $ from this income source: "))
+            date = input("Enter the date this occurred (YYYY-MM-DD): ")
+            finances.add_income(source, amount, date)
+            print("Income added successfully.")
         elif choice == '2':
             category = input("Enter the name of the expense category: ")
-            finances.add_expense_category(category)
+            amount = float(input("Enter the cost ($) of this expense: "))
+            date = input("Enter the date this occurred (YYYY-MM-DD): ")
+            finances.add_expense(category, amount, date)
             print("Expense category added successfully.")
         elif choice == '3':
             date = input("Enter the date of the transaction (YYYY-MM-DD): ")
             category = input("Enter the category of the transaction (income/expense/savings): ")
             amount = float(input("Enter the amount of the transaction: "))
             description = input("Enter a description of the transaction: ")
-            transaction = Transaction(date, category, amount, description)
-            finances.add_transaction(transaction)
+            finances.add_transaction(date, category, amount, description)
             print("Transaction added successfully.")
         elif choice == '4':
             asset = input("Enter the name of the asset: ")
@@ -250,26 +248,33 @@ def main():
         elif choice == '6':
             file_name = input("Enter the file name to save data to: ")
             finances.save_to_file(file_name)
-            print("Data saved successfully.")
         elif choice == '7':
             file_name = input("Enter the file name to load data from: ")
             finances.load_from_file(file_name)
-            print("Data loaded successfully.")
         elif choice == '8':
             amount = float(input("Enter the amount to convert: "))
             from_currency = input("Enter the source currency: ")
             to_currency = input("Enter the target currency: ")
             converted_amount = convert_currency(amount, from_currency, to_currency)
-            print(f"Converted amount: {converted_amount} {to_currency}")
+            if converted_amount is not None:
+                print(f"Converted amount: {converted_amount} {to_currency}")
         elif choice == '9':
-            print("Income Sources:")
+            print("Income:")
             for source in finances.income:
-                print(source)
+                print(f"Source: {source['source']}, Amount: {source['amount']}, Date: {source['date']}")
         elif choice == '10':
-            print("Expense Categories:")
+            print("Expenses:")
             for category in finances.expenses:
-                print(category)
+             print(f"Category: {category['category']}, Amount: {category['amount']}, Date: {category['date']}")
         elif choice == '11':
+            print("Transactions:")
+            for transaction in finances.transactions:
+                print(f"Date: {transaction['date']}, Category: {transaction['category']}, Amount: {transaction['amount']}, Description: {transaction['description']}")
+        elif choice == '12':
+            print("Investments:")
+            for asset, amount in finances.investments.items():
+                print(f"Asset: {asset}, Amount: {amount}")
+        elif choice == '13':
             print("Exiting Financial Management System.")
             sys.exit()
         else:
